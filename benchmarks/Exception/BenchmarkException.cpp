069b77e4769dbf8ff4dfcd5cb4d6cde22f29a403
stefan <stefan.graenitz@gmail.com>
2017-11-25 22:15:29 +0100

    Dump size of error data structures in benchmarks

--- benchmarks/Exception/BenchmarkException.cpp
+++ benchmarks/Exception/BenchmarkException.cpp
@@ -1,6 +1,14 @@
 #include <benchmark/benchmark.h>
+#include <outcome.hpp>
+#include <string>
 
 int main(int argc, char **argv) {
+  printf("sizeof(outcome::result<int>) = %lu\n",
+         sizeof(OUTCOME_V2_NAMESPACE::result<int>));
+
+  printf("sizeof(outcome::result<int, std::string>) = %lu\n",
+         sizeof(OUTCOME_V2_NAMESPACE::result<int, std::string>));
+
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
 }
--- benchmarks/LLVMExpected/BenchmarkLLVMExpected.cpp
+++ benchmarks/LLVMExpected/BenchmarkLLVMExpected.cpp
@@ -1,6 +1,13 @@
 #include <benchmark/benchmark.h>
+#include <Expected.h>
+#include <system_error>
 
 int main(int argc, char **argv) {
+  printf("sizeof(int) = %lu\n", sizeof(int));
+  printf("sizeof(std::error_code) = %lu\n", sizeof(std::error_code));
+  printf("sizeof(llvm::Error) = %lu\n", sizeof(llvm::Error));
+  printf("sizeof(llvm::Expected<int>) = %lu\n", sizeof(llvm::Expected<int>));
+
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
 }
